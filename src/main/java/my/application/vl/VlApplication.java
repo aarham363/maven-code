10c1dee765f0f8a2399aa8ea7d3b7c7222b37b65
Ali <aarham363@gmail.com>
2024-08-31 14:47:53 +0530

    code

---
+++ src/main/java/my/application/vl/VlApplication.java
@@ -0,0 +1,13 @@
+package my.application.vl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/application/vl/VlApplicationTests.java
@@ -0,0 +1,13 @@
+package my.application.vl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
